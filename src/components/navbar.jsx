6e1e299f52b3af46629dd1ceafd3a93ba759687c
Rugie Ann Barrameda <norugie015@gmail.com>
2023-10-11 15:16:33 -0700

    added basic responsive navbar settings

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './navbar.css';
 
 export function Navbar() {
     return(
-        <div className="navbar">
-            <div class="links">
-                <Link to={"/"}>Shop</Link>
-                <Link to={"/cart"}>Cart</Link>
+        <header>
+            <div className="container container-nav">
+                <div className="title">
+                    <h1 className="header">Rugie's Shop</h1>
+                    <p className="header-subtitle">Your One Stop Sample Shop</p>
+                </div>
+                <nav className="navbar">
+                    <ul>
+                        <li><Link to={"/"}>Shop</Link></li>
+                        <li><Link to={"/cart"}>Cart</Link></li>
+                    </ul>
+                </nav>
             </div>
-        </div>
+        </header>
     );
 }
